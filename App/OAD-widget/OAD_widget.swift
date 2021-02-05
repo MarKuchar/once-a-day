2abe68713e12d8dc7e2dab74dce883d31f634ee9
Martin Kuchar <kucharmartin0@gmail.com>
2021-02-04 19:14:50 -0800

    Adjustable time/times

--- App/OAD-widget/OAD_widget.swift
+++ App/OAD-widget/OAD_widget.swift
@@ -12,7 +12,8 @@ import SwiftUI
 struct Provider: TimelineProvider {
     
     let container = CoreDataManager.shared.persistentContainer
-//    lazy var currentCount = getEntry()
+    
+    let txt = ""
     
     func placeholder(in context: Context) -> UserCountEntry {
         
@@ -25,26 +26,25 @@ struct Provider: TimelineProvider {
         let currentCount = counter.currentCount
         return currentCount
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (UserCountEntry) -> ()) {
-//        let currentCount = getEntry()
-        let entry = UserCountEntry(date: Date(), count: 1)
+        let currentCount = getEntry()
+        let entry = UserCountEntry(date: Date(), count: currentCount)
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [UserCountEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
             let currentCount = self.getEntry()
-            print(currentCount)
             let entry = UserCountEntry(date: entryDate, count: currentCount)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -53,7 +53,7 @@ struct Provider: TimelineProvider {
 
 struct WidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
         ZStack {
             Color(UIConstants.bgColor)
@@ -64,39 +64,36 @@ struct WidgetEntryView : View {
                 .opacity(0.2)
             
             VStack {
-//                Text(entry.date, style: .time)
-//                    .font(.caption)
                 
                 Text("\(entry.count)")
                     .font(.custom("NewYorkExtraLarge-Semibold", size: 50))
                     .foregroundColor(Color(UIConstants.customBlue))
                     .padding(.top, 30)
                 
-                Text("times contributed.")
+                Text(entry.count == 1 ? "time contributed." : "times contributed.")
                     .font(.custom("NewYorkLarge-MediumItalic", size: 16))
                     .padding(.top, 10)
             }
-            
         }
     }
-}
-
-@main
-struct OnceADayWidget: Widget {
-    let kind: String = "OnceADayWIdget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            WidgetEntryView(entry: entry)
+    
+    @main
+    struct OnceADayWidget: Widget {
+        let kind: String = "OnceADayWIdget"
+        
+        var body: some WidgetConfiguration {
+            StaticConfiguration(kind: kind, provider: Provider()) { entry in
+                WidgetEntryView(entry: entry)
+            }
+            .configurationDisplayName("My Widget")
+            .description("This is an example widget.")
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
     }
-}
-
-struct OnceADayWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        WidgetEntryView(entry: UserCountEntry(date: Date(), count: 1))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    
+    struct OnceADayWidget_Previews: PreviewProvider {
+        static var previews: some View {
+            WidgetEntryView(entry: UserCountEntry(date: Date(), count: 1))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+        }
     }
 }
